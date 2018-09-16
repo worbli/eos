9a992eee9a2df842e36432fcc320f009c7ffd72c
Robert DeWilder <rob@worbli.io>
2018-09-15 22:58:38 -0400

    removed vote related structs and tables

--- contracts/eosio.system/eosio.system.hpp
+++ contracts/eosio.system/eosio.system.hpp
@@ -80,40 +80,6 @@ namespace eosiosystem {
                         (unpaid_blocks)(last_claim_time)(location) )
    };
 
-   struct voter_info {
-      account_name                owner = 0; /// the voter
-      account_name                proxy = 0; /// the proxy set by the voter, if any
-      std::vector<account_name>   producers; /// the producers approved by this voter if no proxy set
-      int64_t                     staked = 0;
-
-      /**
-       *  Every time a vote is cast we must first "undo" the last vote weight, before casting the
-       *  new vote weight.  Vote weight is calculated as:
-       *
-       *  stated.amount * 2 ^ ( weeks_since_launch/weeks_per_year)
-       */
-      double                      last_vote_weight = 0; /// the vote weight cast the last time the vote was updated
-
-      /**
-       * Total vote weight delegated to this voter.
-       */
-      double                      proxied_vote_weight= 0; /// the total vote weight delegated to this voter as a proxy
-      bool                        is_proxy = 0; /// whether the voter is a proxy for others
-
-
-      uint32_t                    reserved1 = 0;
-      time                        reserved2 = 0;
-      eosio::asset                reserved3;
-
-      uint64_t primary_key()const { return owner; }
-
-      // explicit serialization macro is not necessary, used here only to improve compilation time
-      EOSLIB_SERIALIZE( voter_info, (owner)(proxy)(producers)(staked)(last_vote_weight)(proxied_vote_weight)(is_proxy)(reserved1)(reserved2)(reserved3) )
-   };
-
-   typedef eosio::multi_index< N(voters), voter_info>  voters_table;
-
-
    typedef eosio::multi_index< N(producers), producer_info,
                                indexed_by<N(prototalvote), const_mem_fun<producer_info, double, &producer_info::by_votes>  >
                                >  producers_table;
